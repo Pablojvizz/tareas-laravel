7ec1b67926fb972000c89ff03e4ec5af61af1af9
pablojvizz <vizzari@gmail.com>
2022-09-14 13:53:24 -0300

    primer commit

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+
+class TodosController extends Controller
+{
+    public function store(Request $request){
+        $request->validate([
+            'title' => 'required|min:3'
+        ]);
+        $todo = New Todo;
+        $todo->title = $request->title;
+        $todo->save();
+
+        return redirect()->route('todos')->with('success','Tarea creada correctamente.');
+
+    }
+}
