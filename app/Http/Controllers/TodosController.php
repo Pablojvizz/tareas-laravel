25a783befaf9152fb1630f1e91eb25fad10046fd
pablojvizz <vizzari@gmail.com>
2022-09-15 16:10:11 -0300

    tercer commit

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -19,4 +19,32 @@ class TodosController extends Controller
         return redirect()->route('todos')->with('success','Tarea creada correctamente.');
 
     }
+    public function index(){
+        $todos = Todo::all();
+        return view('todos.index', ['todos' => $todos]);
+
+    }
+    public function show($id){
+        $todo = Todo::find($id);
+        return view('todos.show', ['todo' => $todo]);
+
+    }
+    public function update(Request $request, $id){
+        $request->validate([
+            'title' => 'required|min:3'
+        ]);
+        $todo = Todo::find($id);
+        $todo->title = $request->title;
+        $todo->save();
+
+        return redirect()->route('todos')->with('success','Tarea actualizada!!');
+
+    }
+    public function destroy($id){
+        $todo = Todo::find($id);
+        $todo->delete();
+        $todos = Todo::all();
+        return redirect()->route('todos')->with('success','Tarea a sido eliminada!!');
+
+    }
 }
